c93ed23ce0d98461c2f0367586bdeb176560c00b
ABHIRAM7777777 <abhiram_epuri@srmap.edu.in>
2025-02-13 01:28:51 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.group("abhi");
+let arr = []
+
+var n= 15;
+let sum= 0;
+for(let i=0;i<=n;i++){
+    arr[i] = i;
+    sum += arr[i];
+}
+console.log(sum);
+
+var str = "ihba"
+var rev = ""
+for(let i =str.length-1;i>=0;i--){
+    rev += str[i];
+}
+
+console.log(rev);
